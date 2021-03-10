4cd0b6779f0034603b1d74d2822753f00793d8b1
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-03-10 20:17:27 +1100

    improvements to author names fns

--- Prep/pubs_builder.R
+++ Prep/pubs_builder.R
@@ -1,9 +1,33 @@
 # Functions
 add_auth_tags_to_pubs_df <- function(pubs_df,
+                                     cref_res_ls,
                                      auth_nm_matches_chr,
                                      auth_nm_tag_1L_chr = "admin"){
   pubs_df <- pubs_df %>%
-    dplyr::mutate(auth_tags_chr = AUTHOR %>% 
+    dplyr::mutate(auth_first_family_ls = DOI %>% 
+                    purrr::map(~ if(is.na(.x)){
+                      names_chr <- NA_character_
+                    }else{
+                      cref_res_ls %>%
+                        purrr::pluck(.x) %>%
+                        purrr::pluck("author") %>%
+                        dplyr::mutate(full_nm_chr = paste0(given," ",family)) %>%
+                        dplyr::pull(full_nm_chr)
+                    })) %>%
+    dplyr::mutate(auth_first_family_ls = dplyr::case_when(auth_first_family_ls %>%
+                                                            purrr::map_lgl(~is.na(.x[1])) ~ AUTHOR %>% 
+                                                            purrr::map(~{
+                                                              .x %>% purrr::map_chr(~{
+                                                                ifelse(stringr::str_detect(.x,","),
+                                                                       {
+                                                                         name_chr <- strsplit(.x,",")[[1]] %>% 
+                                                                           trimws()
+                                                                         paste0(name_chr[-1]," ",name_chr[1])
+                                                                       },
+                                                                       .x) %>% stringr::str_replace_all("â€ "," ")
+                                                              })}),
+                                                          T ~ auth_first_family_ls)) %>%
+    dplyr::mutate(auth_tags_chr = auth_first_family_ls %>% 
                     purrr::map_chr(~{
                       paste0(" - ",
                              .x %>% purrr::map_chr(~{
@@ -79,6 +103,12 @@ make_terms_chr <- function(term_1L_chr){
                     c(term_1L_chr) %>% toupper()) %>% paste0(":")))
   return(terms_chr)
 }
+make_nm_combns <- function(given_nm_1L_chr,
+                           middle_nms_chr,
+                           family_nm_1L_chr){
+
+  
+}
 get_auth_surnm_mtchs <- function(authorship_ls,
                                  surname_1L_chr){
   surname_matches_chr <- authorship_ls %>% 
@@ -92,12 +122,16 @@ make_cref_res_ls <- function(doi_chr){ # Add argument for cref polite option
     stats::setNames(doi_chr %>% na.omit() %>% as.vector())
   return(cref_res_ls)
 }
-make_pubs_df <- function(spine_of_pubs_df,
+make_pubs_df <- function(path_to_bib_1L_chr,#spine_of_pubs_df,
+                         auth_nm_matches_chr,
+                         auth_nm_tag_1L_chr = "admin",
                          preprint_srvrs_chr = c("aRXiv","bioRxiv","medRxiv")){
-  spine_of_pubs_df <- make_pubs_df_spine(bib_pubs_df,
-                                         auth_nm_matches_chr = surname_matches_chr[1:length(surname_matches_chr)])
+  spine_of_pubs_df <- bib_pubs_df <- bib2df::bib2df(path_to_bib_1L_chr) 
   cref_res_ls <- make_cref_res_ls(spine_of_pubs_df$DOI)
   pubs_df <- spine_of_pubs_df %>%
+    add_auth_tags_to_pubs_df(cref_res_ls = cref_res_ls,
+                             auth_nm_matches_chr = auth_nm_matches_chr,
+                             auth_nm_tag_1L_chr = auth_nm_tag_1L_chr) %>%
     dplyr::mutate(abstract_tags_chr = ABSTRACT %>% transform_abstract_ls()) %>%
     add_url_tag_vars_to_pubs_df() %>%
     add_keywd_tags_to_pubs_df()%>%
@@ -181,7 +215,7 @@ make_pubs_entries_ls <- function(pubs_df){
                               SUMMARY_PLACEHOLDER <- ..23
                               KEYWORDS_PLACEHOLDER <- ..20
                               TITLE_PLACEHOLDER <- ..3
-                              purrr::map_chr(pubn_md_chr, ~ {
+                              pub_entry_chr <- purrr::map_chr(pubn_md_chr, ~ {
                                 stringr::str_replace(.x,"ABSTRACT_PLACEHODER",ABSTRACT_PLACEHODER) %>% # TRANSFORMED_ABSTRACT
                                   stringr::str_replace("AUTHOR_PLACEHODER",AUTHOR_PLACEHODER) %>%
                                   stringr::str_replace_all("DATE_PLACEHOLDER",DATE_PLACEHOLDER) %>%
@@ -201,6 +235,10 @@ make_pubs_entries_ls <- function(pubs_df){
                                   stringr::str_replace("SUMMARY_PLACEHOLDER",SUMMARY_PLACEHOLDER) %>%
                                   stringr::str_replace("KEYWORDS_PLACEHOLDER",KEYWORDS_PLACEHOLDER) %>%
                                   stringr::str_replace("TITLE_PLACEHOLDER",TITLE_PLACEHOLDER)
+                                index_1L_int <- stringr::str_locate(pub_entry_chr, "# No Keywords") %>% tibble::as_tibble() %>% dplyr::mutate(index_int = 1:length(pub_entry_chr)) %>% na.omit() %>% dplyr::pull(index_int)
+                                if(!identical(integer(0),index_1L_int))
+                                  pub_entry_chr[index_1L_int-1] <- "# tags:"
+                                pub_entry_chr
                               }) 
                             }) %>%
     stats::setNames(make_unique_pub_dir_nms(pubs_df))
@@ -241,7 +279,11 @@ transform_abstract_ls <- function(abstract_ls){
                                        
                         ),
                       ~ ifelse(.y,
-                               stringr::str_replace(.x,stringr::word(.x),paste0("**",stringr::word(.x),"**")),
+                               stringr::str_replace(.x,
+                                                    stringr::word(.x),
+                                                    paste0("**",
+                                                           stringr::word(.x),
+                                                           "**")),
                                .x)
       )
     }) %>%
@@ -258,98 +300,38 @@ transform_abstract_ls <- function(abstract_ls){
 #   tibble::as_tibble() # Update with ready4show ref once migration complete
 
 # Note : need to write script that compacts keywords and place call here
-bib_pubs_df <- bib2df::bib2df("PREP/My_PR_PUBS.bib") 
-surname_matches_chr <- get_auth_surnm_mtchs(bib_pubs_df$AUTHOR,
-                                            "Hamilton")
-pubs_df <- make_pubs_df_spine(bib_pubs_df,
-                   auth_nm_matches_chr = surname_matches_chr[1:length(surname_matches_chr)]) %>%
-make_pubs_df(preprint_srvrs_chr = c("aRXiv", "bioRxiv","medRxiv"))
+# bib_pubs_df <- bib2df::bib2df("PREP/My_PR_PUBS.bib") 
+# surname_matches_chr <- get_auth_surnm_mtchs(bib_pubs_df$AUTHOR,
+#                                             "Hamilton")
+pubs_df <- 
+  # make_pubs_df_spine(bib_pubs_df,
+  #                  auth_nm_matches_chr = surname_matches_chr[1:length(surname_matches_chr)]) %>%
+make_pubs_df(path_to_bib_1L_chr = "PREP/My_PR_PUBS.bib",
+             preprint_srvrs_chr = c("aRXiv", "bioRxiv","medRxiv"))
+
+
 ##
 ##
 ##
 pubn_md_chr <- readLines("Prep/template/index.md")
-entries_ls <- make_pubs_entries_ls(pubs_df)
-entries_ls[[1]] %>% writeLines() # Take from here
-# purrr::map_chr(pubn_md_chr, ~ {
-#   stringr::str_replace(.x,"ABSTRACT_PLACEHODER",bib_pubs_df[idx_int,]$ABSTRACT) %>% # TRANSFORMED_ABSTRACT
-#     stringr::str_replace("AUTHOR_PLACEHODER",
-#                          paste0(" - ",bib_pubs_df[idx_int,]$AUTHOR[[1]], collapse = "\n")) %>%
-#     stringr::str_replace_all("DATE_PLACEHOLDER",
-#                          ifelse(is.na(bib_pubs_df$DOI[idx_int]),
-#                                 "", # Need alternative source for no-doi docs
-#                                 cref_res_ls %>%
-#                                   purrr::pluck(bib_pubs_df$DOI[idx_int]) %>%
-#                            purrr::pluck("created") %>%
-#                            purrr::pluck(2))) %>%
-#     stringr::str_replace("DOI_PLACEHOLDER",
-#                          ifelse(is.na(bib_pubs_df$DOI[idx_int]),
-#                                 "#doi: ",
-#                                 paste0("doi: ",bib_pubs_df$DOI[idx_int]))) %>%
-#     stringr::str_replace("NAME_URL_PLACEHOLDER",
-#                              ifelse(T, # MODIFY
-#                                     paste0("# links:\n# - name: \"\"\n#   url: \"\""),
-#                                     paste0("links:\n - name: \"NAME_GOES_HERE\"\n   url: \"URL_GOES_HERE\"") # MODIFY
-#                                     )) %>%
-#     stringr::str_replace("URL_PDF_PLACEHOLDER",
-#                          ifelse(T, # MODIFY
-#                                 paste0("# url_pdf: ''"),
-#                                 paste0("url_pdf: URL_GOES_HERE") # MODIFY
-#                          )) %>%
-#     stringr::str_replace("URL_CODE_PLACEHOLDER",
-#                          ifelse(T, # MODIFY
-#                                 paste0("# url_dataset: ''"),
-#                                 paste0("url_dataset: 'URL_GOES_HERE'") # MODIFY
-#                          )) %>%
-#     stringr::str_replace("URL_DATASET_PLACEHOLDER",
-#                          ifelse(T, # MODIFY
-#                                 paste0("# url_dataset: ''"),
-#                                 paste0("url_dataset: 'URL_GOES_HERE'") # MODIFY
-#                          )) %>%
-#     stringr::str_replace("URL_POSTER_PLACEHOLDER",
-#                          ifelse(T, # MODIFY
-#                                 paste0("# url_poster: ''"),
-#                                 paste0("url_poster: 'URL_GOES_HERE'") # MODIFY
-#                          )) %>%
-#     stringr::str_replace("URL_PROJECT_PLACEHOLDER",
-#                          ifelse(T, # MODIFY
-#                                 paste0("# url_project: ''"),
-#                                 paste0("url_project: 'URL_GOES_HERE'") # MODIFY
-#                          )) %>%
-#     stringr::str_replace("URL_SLIDES_PLACEHOLDER",
-#                          ifelse(T, # MODIFY
-#                                 paste0("# url_slides: ''"),
-#                                 paste0("url_slides: 'URL_GOES_HERE'") # MODIFY
-#                          )) %>%
-#     stringr::str_replace("URL_SOURCE_PLACEHOLDER",
-#                          ifelse(T, # MODIFY
-#                                 paste0("# url_source: ''"),
-#                                 paste0("url_source: 'URL_GOES_HERE'") # MODIFY
-#                          )) %>%
-#     stringr::str_replace("URL_VIDEO_PLACEHOLDER",
-#                          ifelse(T, # MODIFY
-#                                 paste0("# url_video: '' "),
-#                                 paste0("url_video: 'URL_GOES_HERE'") # MODIFY
-#                          )) %>%
-#     stringr::str_replace("JOURNAL_LONG_PLACEHOLDER",
-#                          bib_pubs_df$JOURNAL[idx_int]) %>%
-#     stringr::str_replace("JOURNAL_SHORT_PLACEHOLDER",
-#                          bib_pubs_df$JOURNAL[idx_int])  %>% #MODIFY
-#     stringr::str_replace("PUB_TYPE_PLACEHOLDER",
-#                          ifelse(bib_pubs_df$CATEGORY[idx_int] == "ARTICLE",
-#                                 ifelse(bib_pubs_df$JOURNAL[idx_int] %in% c("aRXiv",
-#                                                                     "bioRxiv",
-#                                                                     "medRxiv"),
-#                                        "3",
-#                                        "2"),
-#                          "4")) %>%
-#     stringr::str_replace("SUMMARY_PLACEHOLDER",
-#                          corpus::text_split(bib_pubs_df$ABSTRACT[idx_int]) %>%
-#                            dplyr::mutate(text = as.character(text)) %>%
-#                            dplyr::slice(1:2) %>%
-#                            dplyr::pull("text") %>%
-#                            as.vector() %>%
-#                            paste0(collapse=" ") %>%
-#                            trimws()  %>%
-#                            paste0("..")
-#                          )
-# }) %>% writeLines()
+pubs_entries_ls <- make_pubs_entries_ls(pubs_df)
+pub_entries_dir_1L_chr <- "content/publication"
+overwrite_1L_lgl <- F
+
+# index_1L_int <- stringr::str_locate(pub_entry_chr, "# No Keywords") %>% tibble::as_tibble() %>% dplyr::mutate(index_int = 1:length(pub_entry_chr)) %>% na.omit() %>% dplyr::pull(index_int)
+# if(!identical(integer(0),index_1L_int))
+#   pub_entry_chr[index_1L_int-1] <- "# tags:"
+#pub_entry_chr[4] <- pub_entry_chr[4] %>% stringr::str_replace_all("â€ "," ")
+1:2 %>% purrr::walk(~{
+  pub_nm_dir_1L_chr <- names(pubs_entries_ls)[.x]
+  pub_entry_chr <- pubs_entries_ls[[.x]]
+  new_entry_dir_1L_chr <- paste0(pub_entries_dir_1L_chr,"/",pub_nm_dir_1L_chr)
+  new_entry_file_1L_chr <- paste0(new_entry_dir_1L_chr,"/index.md")
+  if(!dir.exists(new_entry_dir_1L_chr))
+    dir.create(new_entry_dir_1L_chr)
+  if(!file.exists(new_entry_file_1L_chr) | overwrite_1L_lgl)
+    pub_entry_chr %>% writeLines(new_entry_file_1L_chr) 
+}
+                      )
+
+
